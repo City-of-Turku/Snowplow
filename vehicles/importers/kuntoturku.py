09dc14b47d87f0e1ca8cc67a49bddb4b80c2c214
Tuomas Haapala <tuomas.haapala@anders.fi>
2017-03-17 01:02:40 +0200

    Add KuntoTurku importer

---
+++ vehicles/importers/kuntoturku.py
@@ -0,0 +1,127 @@
+import logging
+from datetime import datetime
+
+import requests
+from django.conf import ImproperlyConfigured
+from django.db import transaction
+from django.utils import timezone
+
+from vehicles.models import EventType, Location, Vehicle
+
+from .base import BaseVehicleImporter
+
+logger = logging.getLogger(__name__)
+
+# mapping from KuntoTurku events to our supported events
+EVENT_MAPPING = {
+    'Auraus': 'au',
+    'Hiekoitus': 'hi',
+    'Hiekoitus 1': 'hi',
+    'Hiekoitushiekan poisto': 'hn',
+    'Huoltoajo1': None,
+    'Huoltoajo2': None,
+    'Höyläys': 'hs',
+    'Lakaisu1': 'hj',  # TODO correct?
+    'Lakaisu2': 'hj',  # TODO correct?
+    'Lakaisu3': 'hj',  # TODO correct?
+    'Lakaisu4': 'hj',  # TODO correct?
+    'Lehtien keruu': None,
+    'Lehtien murskaus': None,
+    'Maalaus1': None,
+    'Maalaus2': None,
+    'Maalaus3': None,
+    'Maalaus4': None,
+    'Nostintyö': None,
+    'Pesu': 'pe',
+    'pesu1': 'pe',
+    'Polanteen poisto': None,
+    'Roskien keruu1': None,
+    'Roskien keruu2': None,
+    'Roskien keruu3': None,
+    'Roskien keruu4': None,
+    'siirtymäajo': None,
+    'Siirtymäajo': None,
+    'Sorastus': None,
+    'Suolaus': 'su',
+    'SyväSuolaus': 'su',  # TODO correct?
+    'Testiajo': None,
+    'Testiajo2': None,
+    'työajo': None,
+    'Työajo': None,
+    'Virheralueiden hoito': None,
+    'Ylläpito': None,
+    'Ylläpitotyöt': None,
+}
+
+
+class KuntoTurkuImporter(BaseVehicleImporter):
+    id = 'kuntoturku'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.url = self.settings.get('URL')
+        if not self.url:
+            raise ImproperlyConfigured('"URL" is required.')
+
+        self.event_mapping = {
+            key: EventType.objects.get(identifier=value)
+            for key, value in EVENT_MAPPING.items() if value
+        }
+
+    def fetch_data(self):
+        logger.debug('Fetching data from %s' % self.url)
+        response = requests.get(self.url, timeout=self.run_interval / 2.0)
+        response.raise_for_status()
+        return response.json()
+
+    def update_models(self, vehicle_data):
+        logger.debug('Updating models')
+        num_of_new_locations = 0
+
+        for vehicle_datum in vehicle_data:
+            location_datum = vehicle_datum['last_location']
+
+            events = []
+
+            for original_event in location_datum['events']:
+                event = self.event_mapping.get(original_event)
+                if event:
+                    events.append(event)
+                else:
+                    logger.debug('Unknown event %s' % original_event)
+
+            timestamp = timezone.make_aware(datetime.strptime(location_datum['timestamp'], '%Y-%m-%d %H:%M:%S'))
+
+            vehicle, created = Vehicle.objects.get_or_create(
+                data_source=self.data_source,
+                origin_id=vehicle_datum['id'],
+            )
+
+            if created:
+                logger.debug('New vehicle %s' % vehicle)
+
+            location, created = Location.objects.get_or_create(
+                vehicle=vehicle,
+                timestamp=timestamp,
+                defaults=dict(
+                    coords='POINT %s' % location_datum['coords'],
+                )
+            )
+
+            if created:
+                location.events = events
+                num_of_new_locations += 1
+
+        num_of_locations = len(vehicle_data)
+
+        if num_of_new_locations:
+            logger.info('Number of locations %d (%d new)' % (num_of_locations, num_of_new_locations))
+        else:
+            logger.info('No locations')
+
+    def run(self):
+        vehicle_data = self.fetch_data()
+
+        with transaction.atomic():
+            self.update_models(vehicle_data)
--- vehicles/tests/test_importers.py
+++ vehicles/tests/test_importers.py
@@ -1,16 +1,68 @@
+from datetime import datetime
+from unittest.mock import Mock
+
+import pytest
+from django.conf import ImproperlyConfigured
 from django.test import override_settings
+from django.utils import timezone
 
 from vehicles.importers import (
     get_importer_by_id, register_importer, register_importers_from_settings
 )
 from vehicles.importers.base import BaseVehicleImporter
-
+from vehicles.importers.kuntoturku import KuntoTurkuImporter
+from vehicles.models import DataSource, Vehicle
 
 TEST_IMPORTERS = {
     '%s.DummyImporter' % __name__: {
         'URL': 'https://api.dummy.com/v1/',
     },
 }
+
+FETCHED_DATA_1 = [
+    {
+        'id': 123,
+        'machine_type': 'kuorma-auto',
+        'last_location': {
+            'timestamp': '2017-02-18 12:00:00',
+            'coords': '(20.2325767544403 60.3134866561725)',
+            'events': ['Auraus', 'Siirtymäajo']
+        }
+    },
+    {
+        'id': 456,
+        'machine_type': 'kuorma-auto',
+        'last_location': {
+            'timestamp': '2017-02-18 13:00:00',
+            'coords': '(21.2325767544403 61.3134866561725)',
+            'events': ['Auraus', 'Siirtymäajo']
+        }
+    },
+]
+
+FETCHED_DATA_2 = [
+    # THIS HAS NOT BEEN UPDATED
+    {
+        'id': 123,
+        'machine_type': 'kuorma-auto',
+        'last_location': {
+            'timestamp': '2017-02-18 12:00:00',
+            'coords': '(20.2325767544403 60.3134866561725)',
+            'events': ['Auraus', 'Siirtymäajo']
+        }
+    },
+    # THIS HAS BEEN UPDATED
+    {
+        'id': 456,
+        'machine_type': 'kuorma-auto',
+        'last_location': {
+            'timestamp': '2017-02-18 13:15:00',
+            'coords': '(22.2325767544403 62.3134866561725)',
+            'events': ['Pesu', 'foo', 'Hiekoitus']
+        }
+    },
+
+]
 
 
 class DummyImporter(BaseVehicleImporter):
@@ -46,3 +98,61 @@ def test_automatic_registering():
     importer = get_importer_by_id('dummy_importer')
     assert isinstance(importer, DummyImporter)
     assert importer.settings['URL'] == 'https://api.dummy.com/v1/'
+
+
+def test_kunto_turku_importer_url_required():
+    with pytest.raises(ImproperlyConfigured):
+        # URL missing
+        KuntoTurkuImporter({'foo': 'bar'})
+
+
+def test_kunto_turku_importer_basic_import():
+    importer = KuntoTurkuImporter({'URL': 'https://api.dummy.com/v1/'})
+
+    importer.fetch_data = Mock(return_value=FETCHED_DATA_1)
+    importer.run()
+
+    assert Vehicle.objects.count() == 2
+
+    vehicle = Vehicle.objects.get(origin_id='123')
+    assert vehicle.data_source == DataSource.objects.get(id=KuntoTurkuImporter.id)
+    assert vehicle.last_timestamp == timezone.make_aware(datetime(2017, 2, 18, 12, 00, 00))
+
+    assert vehicle.locations.count() == 1
+    location = vehicle.locations.last()
+    assert location.timestamp == timezone.make_aware(datetime(2017, 2, 18, 12, 00, 00))
+    assert round(location.coords.x) == 20
+    assert round(location.coords.y) == 60
+    assert {event.identifier for event in location.events.all()} == {'au'}
+
+
+def test_kuntoturku_importer_two_imports():
+    importer = KuntoTurkuImporter({'URL': 'https://api.dummy.com/v1/'})
+
+    importer.fetch_data = Mock(return_value=FETCHED_DATA_1)
+    importer.run()
+
+    importer.fetch_data = Mock(return_value=FETCHED_DATA_2)
+    importer.run()
+
+    assert Vehicle.objects.count() == 2
+
+    # check that the first vehicle has not been updated
+    first_vehicle = Vehicle.objects.get(origin_id='123')
+    assert first_vehicle.last_timestamp == timezone.make_aware(datetime(2017, 2, 18, 12, 00, 00))
+    assert first_vehicle.locations.count() == 1
+
+    # check that the second vehicle has been updated
+    second_vehicle = Vehicle.objects.get(origin_id='456')
+    assert second_vehicle.last_timestamp == timezone.make_aware(datetime(2017, 2, 18, 13, 15, 00))
+
+    assert second_vehicle.locations.count() == 2
+
+    # should not be affected
+    old_location = second_vehicle.locations.first()
+    assert old_location.timestamp == timezone.make_aware(datetime(2017, 2, 18, 13, 00, 00))
+    assert {event.identifier for event in old_location.events.all()} == {'au'}
+
+    new_location = second_vehicle.locations.last()
+    assert new_location.timestamp == timezone.make_aware(datetime(2017, 2, 18, 13, 15, 00))
+    assert {event.identifier for event in new_location.events.all()} == {'pe', 'hi'}
